1666a2f9d7383110a2e6d16fef578a3c32c623b9
GauravRajpurohit <1762003rajpurohitgaurav@gmail.com>
2026-03-20 11:49:47 +0530

    Phase 2: Update frontend services to call microservices on ports 5001 and 5002

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,10 +1,9 @@
- 
 import axios from 'axios';
 
-const API = process.env.REACT_APP_API_URL + '/api/auth';
+const API = process.env.REACT_APP_AUTH_URL + '/api/auth';
 
-const register = (data) => axios.post(`${API}/register`, data);
-const login    = (data) => axios.post(`${API}/login`, data);
+const register = (data)  => axios.post(`${API}/register`, data);
+const login    = (data)  => axios.post(`${API}/login`, data);
 const getMe    = (token) => axios.get(`${API}/me`, {
   headers: { Authorization: `Bearer ${token}` }
 });
